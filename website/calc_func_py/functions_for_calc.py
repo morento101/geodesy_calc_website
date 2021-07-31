1e32846863d28dfeec67f805ecc82652d31dea28
morento101 <tlcees@gmail.com>
2021-07-31 19:23:32 +0300

    Fixed issues with first function

--- website/calc_func_py/functions_for_calc.py
+++ website/calc_func_py/functions_for_calc.py
@@ -1,33 +1,30 @@
 def coma_replace(string):
+    string = str(string)
     if ',' in string:
-        return string.replace(',', '')
+        string = string.replace(',', '.')
+    return string
 
 
 def y_value_for_zones(zone, distance, direction):
-    try:
-        if type(zone) != int:
-            raise TypeError("Зона повинна бути цілим додатнім числом")
-        elif type(distance) not in [int, float]:
-            raise TypeError("Відстань може бути лише цілим або дробовим додатнім числом")
-        elif type(direction) != str:
-            raise TypeError("Запишіть напрям текстом")
+    if type(zone) != int:
+        raise TypeError("Зона повинна бути цілим додатнім числом")
+    elif type(distance) not in [int, float]:
+        raise TypeError("Відстань може бути лише цілим або дробовим додатнім числом")
+    elif type(direction) != str:
+        raise TypeError("Запишіть напрям текстом")
+    else:
+        if zone <= 0:
+            raise ValueError('Зона може бути лише додатньою')
+        elif distance <= 0:
+            raise ValueError('Зона може бути лише додатньою')
+        elif direction not in ['захід', "схід"]:
+            raise ValueError("Введіть 'захід' або 'схід'")
         else:
-            if zone <= 0:
-                raise ValueError('Зона може бути лише додатньою')
-            elif distance <= 0:
-                raise ValueError('Зона може бути лише додатньою')
-            elif direction not in ['захід', "схід"]:
-                raise ValueError("Введіть 'захід' або 'схід'")
+            zone, distance, direction = int(coma_replace(zone)), float(coma_replace(distance)), coma_replace(direction)
+            if zone > 30:
+                zone -= 30
+            if direction.lower() == "захід":
+                y_value = (zone * 1_000_000) + 500_000 - distance
             else:
-                zone, distance, direction = coma_replace(zone), coma_replace(distance), coma_replace(direction)
-                if zone > 30:
-                    zone -= 30
-                if direction.lower() == "захід":
-                    y_value = (zone * 1_000_000) + 500_000 - distance
-                else:
-                    y_value = (zone * 1_000_000) + 500_000 + distance
-    except Exception as e:
-        return e
-
+                y_value = (zone * 1_000_000) + 500_000 + distance
     return y_value
-
