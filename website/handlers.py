f157a2dda8a0063225a2fc5ba0410b885faef4d3
morento101 <tlcees@gmail.com>
2021-08-17 13:37:45 +0300

    finished page for calculating scale, having segment

---
+++ website/handlers.py
@@ -0,0 +1,22 @@
+from website.models import User
+from flask import Blueprint
+from flask.templating import render_template
+from flask_login import current_user
+
+
+errors = Blueprint('errors', __name__)
+
+
+@errors.app_errorhandler(404)
+def error_404(error):
+    return render_template('errors/404.html', user=current_user), 404
+
+
+@errors.app_errorhandler(403)
+def error_403(error):
+    return render_template('errors/403.html', current_user), 403
+
+
+@errors.app_errorhandler(500)
+def error_500(error):
+    return render_template('errors/500.html', current_user), 500
