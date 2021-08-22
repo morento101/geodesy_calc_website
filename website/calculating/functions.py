a810e815c731a87cb249568abe4274415fd302f7
Yaroslav <80165970+morento101@users.noreply.github.com>
2021-08-22 13:31:47 +0300

    update

--- website/calculating/functions.py
+++ website/calculating/functions.py
@@ -17,7 +17,7 @@ def y_value_for_zones(zone, distance, direction):
             raise ValueError('Зона може бути лише додатньою')
         elif distance <= 0 or distance > 999_999:
             raise ValueError('Відстань може бути лише додатньою та не більшою ніж 999 999')
-        elif direction not in ['захід', "схід"]:
+        elif direction.lower() not in ['захід', "схід"]:
             raise ValueError("Введіть 'захід' або 'схід'")
         else:
             if zone > 30:
